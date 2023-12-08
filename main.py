4e749a3ef8a877914b2046e56d2eea42c1704bb2
Doge Gui <guiqiqi187@gmail.com>
2023-12-08 10:35:12 +0300

    Update UI

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-"""main.py"""
+"""Calculator in RPN."""
 
 import calculator
 from ui import UI
 
+
 def evaluator(expression: str) -> calculator.Number:
     """Calculate expression and return answer."""
+    print(f'Expression: {expression}')
     tokens = list(calculator.tokenize(expression))
+    print(f'Tokenized: {tokens}')
     calculator.prefixing(tokens)
     calculator.balancing(tokens)
+    print(f'Validated tokens: {tokens}')
     tokens = list(calculator.shunting(tokens))
+    print(f'RPN tokens: {tokens}')
     return calculator.evaluate(tokens)
 
 
--- ui.py
+++ ui.py
@@ -89,6 +89,22 @@ class UI(tkinter.Tk):
             if event.keysym == 'Return':
                 self.calculate()
 
+        def insert(char: str) -> None:
+            """Insert a new char to current position."""
+            current = self.inputs.get()
+            position = self.buffer.index(tkinter.INSERT)
+            updated = current[:position] + char + current[position:]
+            self.inputs.set(updated)
+            self.buffer.icursor(position + 1)
+
+        def remove() -> None:
+            """Remove a char in current cursor position."""
+            current = self.inputs.get()
+            position = self.buffer.index(tkinter.INSERT)
+            updated = current[:position - 1] + current[position:]
+            self.inputs.set(updated)
+            self.buffer.icursor(position - 1)
+
         buffer = self.buffer = tkinter.Entry(
             self, textvariable=self.inputs, justify=tkinter.RIGHT, font=("Calibri 20"))
         buffer.grid(row=0, column=0, sticky=tkinter.NSEW, ipady=10)
@@ -96,11 +112,13 @@ class UI(tkinter.Tk):
 
         InputPad(
             self,
-            add=lambda s: self.inputs.set(self.inputs.get() + s),
-            remove=lambda: self.inputs.set(self.inputs.get()[:-1]),
+            add=insert,
+            remove=remove,
             clear=lambda: self.inputs.set(''),
             evaluator=self.calculate
         ).grid(row=2, column=0)
+
+        self.buffer.grab_set()
 
     def calculate(self) -> None:
         """Get expression saved in inputs buffer and calculate for updating result."""
@@ -108,7 +126,8 @@ class UI(tkinter.Tk):
         self.inputs.set('')
         try:
             answer = self.evaluator(expression)
-            self.inputs.set(str(answer).rstrip('.0'))
+            answer = int(answer) if int(answer) == answer else round(answer, 5)
+            self.inputs.set(str(answer))
             self.buffer.icursor(len(self.inputs.get()))
         except ValueError as error:
             msgbox.showerror('Invalid expression', error.args[0])
